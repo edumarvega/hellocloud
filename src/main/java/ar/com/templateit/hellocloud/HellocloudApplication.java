58f3a10b3d734e1bf28a96ecfa32bbd670aeb9cd
Martin Vega <martin.vega@osde.com.ar>
2024-08-17 10:36:11 -0300

    haciendo commit

---
+++ src/main/java/ar/com/templateit/hellocloud/HellocloudApplication.java
@@ -0,0 +1,13 @@
+package ar.com.templateit.hellocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellocloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/templateit/hellocloud/HellocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.templateit.hellocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
